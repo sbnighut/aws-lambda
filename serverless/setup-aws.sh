5b2f7e94062ca3420f0d2e360ce7dfc6aa9fb38a
snighut <Sw@8Sw@8>
2019-03-09 21:57:09 -0700

    Added language specific functions for running in AWS Lambda

---
+++ serverless/setup-aws.sh
@@ -0,0 +1,28 @@
+npm install -g serverless
+
+serverless config credentials --provider aws --key AKIAIPZ6KY3Y4GS4ASUQ --secret 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 --profile serverless-admin
+
+**Create and deploy lambda function from command line:**
+sls create --template aws-python --path hello-world-python
+
+**Deploy a lambdafunction:**
+sls deploy -v
+
+**Executing lambda function and handling it in hello handler:**(argument -l stands for showing logs on console)
+sls invoke -f hello -l
+
+**Only deploy function and not the stack:**
+sls deploy function -f hello
+
+
+**Check continuous logs :**
+sls logs -f hello -l 
+
+**:**
+**:**
+**:**
+**:**
+**:**
+**:**
+**:**
+**:**
